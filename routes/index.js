3e04ed6b7819a2e949c37b3d74c233b561a28a55
Marcelo Bohn <marcelo.bohn@gmail.com>
2015-09-03 10:00:16 -0400

    test routes and params

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,17 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  //res.send('render sem view por get');
+});
+
+router.post('/', function(req, res){
+  var name = req.body.name;
+  res.send('render sem view por post. Name: ' + name);
+});
+
+router.get('/find/:text', function(req, res){
+  var text = req.params.text;
+  res.send('param get url: ' + text);
 });
 
 module.exports = router;
